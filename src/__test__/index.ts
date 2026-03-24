e4e441aa754f85a3493d09f72c2b4eae873ade57
unknown <evertonjose421@gmail.com>
2026-03-24 12:34:31 -0300

    24/03 12:34

--- src/__test__/index.ts
+++ src/__test__/index.ts
@@ -1,8 +1,23 @@
 import { selectPedidoSistema } from "../repository/repository-pedido.ts";
+import { serviceSendOrder } from "../services/service-send-orders.ts";
 
  
- 
- 
- const a = await selectPedidoSistema(2) 
- console.log(a);
- 
+const dataOrders = await selectPedidoSistema();
+                if(dataOrders.length > 0  ){
+                    for( const i of dataOrders ){
+                      await serviceSendOrder({  
+                            criado_em: i.DATA_RECAD,
+                                dados_json: String(i),
+                                id:0,
+                                id_evento:0,
+                                id_message: '0',
+                                id_registro: i.CODIGO,
+                                setor: 0,
+                                status:'PROCESSADO',
+                                tabela: 0,
+                                tabela_origem:'cad_orca',
+                                tipo_evento:'INSERT'
+                            })
+                    }
+
+                }
